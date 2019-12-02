ab25ff546b980ddb53a93444d179068febc81dda
Vova Kalinichenko <redline711@gmail.comm>
2019-12-03 01:22:55 +0200

    3.2 Add event

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
     pageTitle: 'React components'
   }
 
+  changeTitleHandler = () => {
+    console.log('Clicked')
+  }
+
   render() {
     const divStyle = {
       textAlign: 'center'
@@ -21,6 +25,9 @@ class App extends Component {
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
+
+        <button onClick={this.changeTitleHandler}>Change title</button>
+
         <Car name={cars[0].name} year={cars[0].year}/>
         <Car name={cars[1].name} year={cars[1].year}/>
         <Car name={cars[2].name} year={cars[2].year}/>
