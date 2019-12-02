d3da0eb1f8f0487d96b1d4660713844f4fa8d04f
Vova Kalinichenko <redline711@gmail.comm>
2019-12-02 02:44:56 +0200

    2.3 Inline styles

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const divStyle = {
+    textAlign: 'center'
+  }
   return (
-    <div>
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-      <p>Hello</p>
+    <div style={divStyle}>
+      <h1 style={{color: 'blue', fontSize: '20px'}}>Hello World!</h1>
     </div>
   );
-  // return React.createElement(
-  //   'div',
-  //   {
-  //     className: 'App'
-  //   },
-  //   React.createElement(
-  //     'h1',
-  //     null,
-  //     'Hello World!'),
-  // )
 }
 
 export default App;
