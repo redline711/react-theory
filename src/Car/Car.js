2f9481631df41002f9084dcfba7846a40b7cec38
Vova Kalinichenko <redline711@gmail.comm>
2019-12-02 18:36:05 +0200

    2.5 dynamic data output

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,26 +1,7 @@
 import React from 'react';
-
-// function Car() {
-//     return (
-//         <h2>This is car component</h2>
-//     );
-// }
-
-// const Car = () => {
-//     return (
-//         <h2>This is car component</h2>
-//     )
-// }
-
-// const Car = () => 
-// <div>
-//     This is car component
-//     <strong>TEST</strong>
-// </div>
-
 export default () => (
     <div>
-        This is car component
-        <strong>TEST</strong>
+        <p>This is car component</p>
+        <p>Number: <strong>{Math.round(Math.random() *100)}</strong></p>
     </div>
 );
