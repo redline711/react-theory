dce9bad2e08560c64a6e9cb573c4a8eb8555206a
Vova Kalinichenko <redline711@gmail.comm>
2019-12-03 01:29:53 +0200

    3.3 Change state

--- src/App.js
+++ src/App.js
@@ -14,7 +14,15 @@ class App extends Component {
   }
 
   changeTitleHandler = () => {
-    console.log('Clicked')
+
+    const oldTitle = this.state.pageTitle
+
+    const newTitle = oldTitle + ' (changed) '
+    
+    this.setState({
+      pageTitle: newTitle
+    })
+
   }
 
   render() {
