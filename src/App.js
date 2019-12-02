917ab0ae2a674f3594fc3301066ff9e07a200621
Vova Kalinichenko <redline711@gmail.comm>
2019-12-02 20:25:55 +0200

    2.7 Transmission content

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ function App() {
     <div style={divStyle}>
       <h1>Hello World!</h1>
       <Car name={'Ford'} year={2018}/>
-      <Car name={'Audi'} year={2016}/>
-      <Car name={'Mazda'} year={2010}/>
+      <Car name={'Audi'} year={2016}>
+        <p style={{color: 'red'}}>COLOR</p>
+      </Car>
+      <Car name={'Mazda'} year={2010}>
+        <p style={{color: 'green'}}>COLOR</p>
+      </Car>
     </div>
   );
 }
