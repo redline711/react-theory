db164c3801ecf5e2fe0c11c0fc8528a8d064602e
Vova Kalinichenko <vova.kalinichenko@dreamhost.com>
2020-01-12 19:50:53 +0200

    5.5 Life circle changes

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import Radium from 'radium'
 
 class Car extends React.Component {
+	componentWillReceiveProps(nextProps) {
+		console.log('Car componentWillReceiveProps', nextProps)
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('Car shouldComponentUpdate', nextProps, nextState)
+		return nextProps.name.trim() !== this.props.name.trim()
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		console.log('Car componentWillUpdate', nextProps, nextState)
+	}
+
+	componentDidUpdate() {
+		console.log('componentDidUpdate')
+	}
+
 	render() {
+		console.log('Car render')
 		const inputClasses = ['input']
 
 		if (this.props.name !== '') {
@@ -42,4 +59,4 @@ class Car extends React.Component {
 	}
 }
 
-export default Radium(Car)
+export default Car
