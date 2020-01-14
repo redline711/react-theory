af66a074fbf06f98cf0b91a39f95bcf847e3d8fb
Vova Kalinichenko <vova.kalinichenko@dreamhost.com>
2020-01-14 04:46:21 +0200

    5.12 Leading into components of higher of order

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,43 +1,9 @@
 import React from 'react'
+import './Car.css'
+import withClass from '../hoc/withClass'
 
 class Car extends React.Component {
-	componentWillReceiveProps(nextProps) {
-		console.log('Car componentWillReceiveProps', nextProps)
-	}
-
-	shouldComponentUpdate(nextProps, nextState) {
-		console.log('Car shouldComponentUpdate', nextProps, nextState)
-		return nextProps.name.trim() !== this.props.name.trim()
-	}
-
-	componentWillUpdate(nextProps, nextState) {
-		console.log('Car componentWillUpdate', nextProps, nextState)
-	}
-
-	// static getDerivedStateFromProps(nextProps, prevState) {
-	// 	console.log('Car getDerivedStateFromProps', nextProps, prevState)
-	// 	return prevState
-	// }
-
-	componentDidUpdate() {
-		console.log('componentDidUpdate')
-	}
-
-	// getSnapshotBeforeUdate() {
-	// 	console.log('Car getSnapshotBeforeUdate')
-	// }
-
-	componentWillUnmount() {
-		console.log('Car componentWillUnmount')
-	}
-
 	render() {
-		console.log('Car render')
-
-		// if (Math.random() > 0.7) {
-		// 	throw new Error('Car random failed')
-		// }
-
 		const inputClasses = ['input']
 
 		if (this.props.name !== '') {
@@ -50,17 +16,8 @@ class Car extends React.Component {
 			inputClasses.push('bold')
 		}
 
-		const style = {
-			border: '1px solid #ccc',
-			boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-			':hover': {
-				border: '1px solid #aaa',
-				boxShadow: '0 4px 16px 0 rgba(0, 0, 0, .25)'
-			}
-		}
-
 		return (
-			<div className="Car" style={style}>
+			<React.Fragment>
 				<h3>Car name: {this.props.name}</h3>
 				<p>
 					Year: <strong>{this.props.year}</strong>{' '}
@@ -72,9 +29,9 @@ class Car extends React.Component {
 					className={inputClasses.join(' ')}
 				/>
 				<button onClick={this.props.onDelete}>Delete</button>
-			</div>
+			</React.Fragment>
 		)
 	}
 }
 
-export default Car
+export default withClass(Car, Car)
